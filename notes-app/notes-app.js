d113bed19cfe4ca425d67bf43a49ff17933bf28a
deddythok <deddy.donchild@gmail.com>
2025-01-28 15:36:25 +0700

    javascript date

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -37,3 +37,27 @@ window.document.addEventListener('storage', function(e){
         renderNotes(notes, filters);
     }
 });
+
+const now = new Date();
+const timestamp = now.getTime();
+
+const myDate = new Date(timestamp);
+console.log(myDate.getFullYear());
+
+// console.log(`Year: ${now.getFullYear()}`);
+// console.log(`Month: ${now.getMonth()}`);
+// console.log(`Day: ${now.getDate()}`);
+// console.log(`Hour: ${now.getHours()}`);
+// console.log(`Minutes: ${now.getMinutes()}`);
+// console.log(`Seconds: ${now.getSeconds()}`);
+
+const dateOne = new Date('March 1 2025 12:00:00');
+const dateTwo = new Date();
+const dateOneTimestamp = dateOne.getTime();
+const dateTwoTimestamp = dateTwo.getTime();
+
+if(dateOneTimestamp < dateTwoTimestamp){
+    console.log(dateOne.toString());
+} else if(dateTwoTimestamp < dateOneTimestamp){
+    console.log(dateTwo.toString());
+}
